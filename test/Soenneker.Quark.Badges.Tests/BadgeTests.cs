b5763506da79dbf40aa58c08572b066aaa442ece
Jake Soenneker <jake@soenneker.com>
2025-09-12 15:50:08 -0500

    Initial commit

---
+++ test/Soenneker.Quark.Badges.Tests/BadgeTests.cs
@@ -0,0 +1,18 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Badges.Tests;
+
+[Collection("Collection")]
+public sealed class BadgeTests : FixturedUnitTest
+{
+    public BadgeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
